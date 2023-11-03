3479cecc18d6cf87901efe54ade2599ed7823308
jiyy25 <jiyy825@gmail.com>
2023-11-03 17:53:41 +0900

    스와이퍼 제이쿼리

--- js/web.js
+++ js/web.js
@@ -1,4 +1,13 @@
 $(document).ready(function () {
     //화면이 준비되면 실행해라.
 
+    setInterval(function () {
+        $(".swiper-wraper").animate({ "marginLeft": -1200 }, 400, function () {
+            $("div:first-child", this).appendTo($(this))
+            //this 안의 첫번째 div를 this의 맨 뒤로 추가...
+            $(this).css("marginLeft", 0)
+        })
+    }, 3000)
+
+
 })
