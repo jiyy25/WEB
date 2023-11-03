dba8e1a6daf3afad1086fa7e5294c5a4676c6b17
jiyy25 <jiyy825@gmail.com>
2023-11-03 17:57:22 +0900

    스와이퍼 세로

--- js/web.js
+++ js/web.js
@@ -2,10 +2,10 @@ $(document).ready(function () {
     //화면이 준비되면 실행해라.
 
     setInterval(function () {
-        $(".swiper-wraper").animate({ "marginLeft": -1200 }, 400, function () {
+        $(".swiper-wraper").animate({ "marginTop": -300 }, 400, function () {
             $("div:first-child", this).appendTo($(this))
             //this 안의 첫번째 div를 this의 맨 뒤로 추가...
-            $(this).css("marginLeft", 0)
+            $(this).css("marginTop", 0)
         })
     }, 3000)
 
